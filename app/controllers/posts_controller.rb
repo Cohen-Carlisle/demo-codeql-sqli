f0e2ea8602071eebb0a2b92b39e7c3e804767df2
Cohen Carlisle <cohencarlisle+github@pm.me>
2023-11-02 14:37:14 -0400

    try slice on params

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -60,7 +60,7 @@ class PostsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_post
-      @post = Post.find_by(params.permit(:id))
+      @post = Post.find_by(params.permit(:id).slice(:id))
     end
 
     # Only allow a list of trusted parameters through.
